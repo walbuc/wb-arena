2ccf7edd5487028467c8bb329194c262767dc4ca
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2024-09-09 08:23:55 -0300

    update server

--- server/index.ts
+++ server/index.ts
@@ -51,7 +51,7 @@ app.all(
 )
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
+app.listen(Number(port), '0.0.0.0', () => {
   console.log(`Express server listening on port ${port}`)
 })
 
